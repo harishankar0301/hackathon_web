70dd32f9958661bb0ae2ca8a3e764d423f8864e3
Harishankar <harishankar0301@gmail.com>
2021-03-05 19:53:14 +0530

    change pwd

--- src/app/change-password/change-password.component.ts
+++ src/app/change-password/change-password.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 @Component({
   selector: 'app-change-password',
   templateUrl: './change-password.component.html',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChangePasswordComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
+  submitform(form) {
 
+    console.log(form.value);
+    this.http.post('http://localhost:3000/api/reset', { email: form.value.emailId }).subscribe(res => {
+      alert("Mail sent!!");
+    })
+
+
+  }
 }
