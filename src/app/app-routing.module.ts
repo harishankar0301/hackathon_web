89a744d4fd06ffc7762c3d5d3efdbf36e8fcb3fa
Adhitya-Raj <Adhitya-Raj@github.com>
2021-03-05 18:44:20 +0530

    Polished UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { ChangePasswordComponent } from './change-password/change-password.component';
+import { UploadPhotoComponent } from './upload-photo/upload-photo.component';
+import { ViewPhotoComponent } from './view-photo/view-photo.component';
+const routes: Routes = [
+  {path: '', component: SignupComponent},
+  {path: 'signup', component: SignupComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'upload', component: UploadPhotoComponent},
+  {path: 'view', component: ViewPhotoComponent},
+  {path: 'logout', redirectTo: ''},
+  {path: 'changepassword', component:ChangePasswordComponent},
+  {path: '**', component: SignupComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
